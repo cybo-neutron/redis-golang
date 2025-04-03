56b641807e3696753f5c9e38b0c8810b041f730b
neer <neer@email.com>
2025-04-03 09:54:23 +0530

    pub-sub

--- cmd/main.go
+++ cmd/main.go
@@ -2,25 +2,23 @@ package main
 
 import (
 	"caching/examples/messaging"
-	"caching/internal/redis"
 	"caching/internal/routes"
-	"context"
-	"fmt"
 )
 
 func main() {
-	ctx := context.Background()
-
-	client, err := redis.New("localhost:6379")
-	if err != nil {
-		fmt.Println("Error establishing connection", err)
-	}
+	// ctx := context.Background()
+	//
+	// client, err := redis.New("localhost:6379")
+	// if err != nil {
+	// 	fmt.Println("Error establishing connection", err)
+	// }
 
 	// examples.StringExamples(client, &ctx)
 	// examples.HashExample(client, &ctx)
 	// examples.SetExample(client, &ctx)
 	// messaging.MessagingExample(client, &ctx)
-	messaging.ListPubSubExample(client, &ctx)
+	// messaging.ListPubSubExample(client, &ctx)
+	messaging.PubSubExample()
 
 	// challenges.StringHashChallenge(*client, &ctx)
 
--- examples/messaging/pub_sub_example.go
+++ examples/messaging/pub_sub_example.go
@@ -4,12 +4,31 @@ import (
 	"caching/internal/redis"
 	"context"
 	"fmt"
+	"time"
+
+	go_redis "github.com/redis/go-redis/v9"
 )
 
 const PUB_SUB_CHANNEL = "pub_sub_channel"
 
+var ctx = context.Background()
+
+func InitSubcriber(client *go_redis.Client) {
+	fmt.Println("Initializing subscriber")
+	subscriber := client.Subscribe(ctx, PUB_SUB_CHANNEL)
+	defer subscriber.Close()
+
+	for {
+		msg, err := subscriber.ReceiveMessage(ctx)
+		if err != nil {
+			fmt.Println("Error while receiving message : ", err)
+			panic(err)
+		}
+		fmt.Println("Received message : ", msg.Channel, msg.Payload)
+	}
+}
+
 func PubSubExample() {
-	ctx := context.Background()
 	client, err := redis.New("localhost:6379")
 	if err != nil {
 		fmt.Println("Error instantiating client")
@@ -19,5 +38,10 @@ func PubSubExample() {
 	_, err = client.Ping(ctx).Result()
 	if err != nil {
 		fmt.Println("Error in connection", err)
+		return
 	}
+
+	go InitSubcriber(client)
+	time.Sleep(10 * time.Second)
+	client.Publish(ctx, PUB_SUB_CHANNEL, "test-message")
 }
